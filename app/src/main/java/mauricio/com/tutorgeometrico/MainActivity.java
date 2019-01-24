379c8cca676d0658e6eb475b37a8152e9f2bba85
mauricio-morais <m4umorais@gmail.com>
2019-01-23 23:56:14 -0200

    Configurada biblioteca 'codescanner' para leitura de QR Code

--- app/src/main/java/mauricio/com/tutorgeometrico/MainActivity.java
+++ app/src/main/java/mauricio/com/tutorgeometrico/MainActivity.java
@@ -1,13 +1,30 @@
 package mauricio.com.tutorgeometrico;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    AppCompatButton btnOpenScanner = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnOpenScanner = findViewById(R.id.btnOpenScanner);
+
+        btnOpenScanner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(MainActivity.this, ScannerCode.class));
+
+            }
+        });
+
     }
 }
